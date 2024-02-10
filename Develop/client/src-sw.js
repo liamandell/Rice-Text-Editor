ea916f0c3b5da8d94d9d5cf02751ee15b48b0640
liamandell <liamandell14@gmail.com>
2024-02-10 12:46:33 -0600

    asset caching

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,5 +27,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
 
 
